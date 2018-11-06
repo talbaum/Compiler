7897e0dad73306d44aac2c47cb118c0ba13cf4cb
Tal Baum <talbaum16@gmail.com>
2018-11-06 17:11:45 +0200

    Add files via upload

--- reader.ml
+++ reader.ml
@@ -54,26 +54,42 @@ let read_sexprs string = raise X_not_yet_implemented;;
   
 (* ------------------- Boolean Parser ------------------------------ *)
 
-let _sulamit_ =  (PC.char '#');;
 
+#use "pc.ml";;
 let _tParser_ = (PC.char 't');;
 
 let _fParser_= (PC.char 'f');;
 
-let _trueParser_ = PC.caten _sulamit_ _tParser_;;
+let _trueParser_ =
+  let _sulamit_ =  (PC.char '#') in
+  let _truep_ = PC.caten _sulamit_ _tParser_ in
+  PC.pack _truep_ (fun(s,t) -> Bool(true));;
 
-let _falseParser_= PC.caten _sulamit_ _fParser_;;
+let _falseParser_ =
+  let _sulamit_ =  (PC.char '#') in
+  let _falsep_ = PC.caten _sulamit_ _fParser_ in
+  PC.pack _falsep_ (fun(s,f) -> Bool(false));;
 
-let _booleanParser_ = PC.disj _trueParser_ _falseParser_
+let _booleanParser_ = PC.disj _trueParser_ _falseParser_;;
+
+PC.test_string _booleanParser_ "#t";;
+
+let _Sexpr_ = PC.disj
                     
 (* ------------------------------------------------------------------ *)
 
-
+#use "pc.ml";;
 let _digit_ = PC.range '0' '9' ;;
+let _dot_ = PC.char '.';;
 
-
-let _numberParser_ =
+let _integerParser_ =
   let _digits_ = PC.plus _digit_ in
-  PC.pack _digits_ (fun (ds) -> Number (int_of_string (list_to_string ds)));;
+  PC.pack _digits_ (fun (ds) -> Number (Int((int_of_string (list_to_string ds)))));;
+
+let _floatParser_=
+  let _digits_ = PC.plus _digit_ in
+  PC.pack _digits_ _dot_ _digits_ (fun (ds dot ds2) -> float_of_string ((list_to_string ds) ^ "." ^(list_to_string ds2))));;
+
+let _numberParser_ = PC.disj_list [_floatParser_ ; _integerParser_;];;
     
 end;; (* struct Reader *)
