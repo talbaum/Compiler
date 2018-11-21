1d9ad50e7667a8c0d1bc4e69c489e0c217d36402
Tal Baum <talbaum16@gmail.com>
2018-11-21 18:32:33 +0200

    Update tag-parser.ml

--- tag-parser.ml
+++ tag-parser.ml
@@ -46,13 +46,16 @@ let rec expr_eq e1 e2 =
      (expr_eq e1 e2) &&
        (List.for_all2 expr_eq args1 args2)
   | _ -> false;;
-	
+  
                        
 exception X_syntax_error;;
 
 module type TAG_PARSER = sig
   val tag_parse_expression : sexpr -> expr
   val tag_parse_expressions : sexpr list -> expr list
+  val test : string -> expr
+  val test_reader : string -> sexpr
+
 end;; (* signature TAG_PARSER *)
 
 module Tag_Parser : TAG_PARSER = struct
@@ -63,7 +66,9 @@ let reserved_word_list =
    "quasiquote"; "quote"; "set!"; "unquote";
    "unquote-splicing"];;  
 
-(* work on the tag parser starts here *)
+(* ------------------------ work on the tag parser starts here --------------------------------------*)
+
+
 let is_in_reserved_list = function
   | Symbol(check_me)->   List.mem check_me reserved_word_list 
   | _-> raise X_not_yet_implemented;;
@@ -78,6 +83,15 @@ let rec find_last_element = function
   | _::xs -> find_last_element xs
   | [] -> raise X_syntax_error;;
 
+
+let rec convert_to_sexpr_list list = match list with
+| Nil -> []
+| Pair(car, Nil)->[car]
+(*| car -> [car]*)
+| Pair(car,cdr) ->  car :: (convert_to_sexpr_list cdr)
+| _ -> raise X_not_yet_implemented;;
+
+
 let rec convert_to_string_list list = match list with
 | Nil -> []
 | Pair(Symbol(car), Nil)-> if (is_in_reserved_list(Symbol(car)))  then raise X_not_yet_implemented else [car]
@@ -86,10 +100,10 @@ let rec convert_to_string_list list = match list with
 | _ -> raise X_not_yet_implemented;;
 
 
+
 let is_not_duplicated_args args = 
 let unique_number_of_args = (List.sort_uniq String.compare args) in
 if (List.length unique_number_of_args == List.length args) then true else false;;
-
 
 let rec tag_parse sexpr =  match sexpr with
 | Number (Int(a)) -> Const(Sexpr(Number(Int(a))))
@@ -98,36 +112,81 @@ let rec tag_parse sexpr =  match sexpr with
 | Char(a)-> Const(Sexpr(Char(a)))
 | String(a)-> Const (Sexpr(String(a)))
 | Pair(Symbol("quote"), Pair(a, Nil)) -> Const(Sexpr(a))
-| Symbol(a)->  if( List.mem a reserved_word_list)  then raise X_not_yet_implemented else Var(a)
+| Symbol(a)-> if( List.mem a reserved_word_list)  then raise X_not_yet_implemented else Var(a)
 | Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil)))) ->
   If(tag_parse test, tag_parse dit, tag_parse dif)
 | Pair(Symbol("if"), Pair(test, Pair(dit, Nil)))->
   If(tag_parse test, tag_parse dit, Const (Void))
-| Pair(Symbol("lambda"), Pair(args, body)) -> (match args with 
-    | Nil -> LambdaSimple ([], tag_parse body)
+|Pair(Symbol("define"),(Pair(Symbol(name) ,(Pair(expr, Nil)))))-> define_tag_parser name expr
+|Pair(Symbol("set!"),(Pair(Symbol(name) ,(Pair(expr, Nil)))))->set_tag_parser name expr
+|Pair(Symbol("begin"), Pair(exprs,Nil))-> seq_tag_parser exprs
+|Pair(Symbol("or"),Pair(exprs,Nil))->or_tag_parser exprs
+| Pair(Symbol("lambda"), Pair(args, body)) -> lambda_tag_parser args body
+  | Pair(Symbol "let",Pair(Pair(rib, ribs), Pair(body, Nil))) ->raise X_not_yet_implemented
+|Pair (Symbol (functionName), Pair(args , Nil))->applic_tag_parser functionName args
+| _ -> raise X_syntax_error 
+
+and lambda_tag_parser args body= 
+(match args with 
+    |Symbol(vs) -> LambdaOpt([], vs ,seq_tag_parser body)
     | Pair(car,cdr) -> let converted_args = convert_to_string_list args in 
                       if (is_not_duplicated_args converted_args) then
                               if(is_improper_list args)
-                              then LambdaOpt(converted_args, find_last_element(converted_args), tag_parse body)
-                              else LambdaSimple(converted_args, tag_parse body)
+                              then LambdaOpt(converted_args, find_last_element(converted_args), seq_tag_parser body)
+                              else LambdaSimple(converted_args, seq_tag_parser body)
                       else raise X_not_yet_implemented
-    |vs ->LambdaOpt([],find_last_element(convert_to_string_list vs),tag_parse body)) 
-  | Pair(Symbol "let",Pair(Pair(rib, ribs), Pair(body, Nil))) ->raise X_not_yet_implemented
-  | _ -> raise X_not_yet_implemented;;
+|_ -> raise X_not_yet_implemented) 
+(* ------------------------------- map -------------------------------------*)
+
+and map_tag_parse args = 
+List.map tag_parse (convert_to_sexpr_list args)
+
+
+(*---------------------------- or  ----------------------------------------*)
+and or_tag_parser exprs= 
+let tagParsedExprs = map_tag_parse exprs in
+(match tagParsedExprs with
+    |[]->Const(Sexpr(Bool(false)))
+    |[tagParsedExprs]-> tagParsedExprs
+    |_->Or(tagParsedExprs))
+
+(*--------------------------- seq --------------------------------------------- *)
+and seq_tag_parser exprs = 
+let tagParsedExprs = map_tag_parse exprs in
+(match tagParsedExprs with
+    |[]->Const(Void)
+    |[tagParsedExprs]-> tagParsedExprs
+    |_->Seq(tagParsedExprs))
+
+(*--------------------------- Applic ------------------------------------------- *)
+
+and applic_tag_parser functionName args = 
+let tag_parsed_functionName = tag_parse (Symbol(functionName)) in
+let tag_parsed_args = map_tag_parse args in
+Applic(tag_parsed_functionName ,tag_parsed_args) 
+
+(*------------------------------ Define ----------------------------------------*)
+and define_tag_parser name expr =
+let tag_parsed_name = tag_parse(Symbol(name)) in
+ let tag_parsed_expr = tag_parse expr in
+ Def(tag_parsed_name,tag_parsed_expr)
+(*------------------------------ Set ----------------------------------------*)
+
+and set_tag_parser name expr = 
+let tag_parsed_name = tag_parse(Symbol(name)) in
+let tag_parsed_expr = tag_parse expr in
+Set(tag_parsed_name,tag_parsed_expr);;
 
 
 let tag_parse_expression sexpr = tag_parse sexpr;;
-let tag_parse_expressions sexpr = raise X_not_yet_implemented;;
+let tag_parse_expressions sexpr = List.map tag_parse_expression sexpr;;
 
-tag_parse_expression (Reader.read_sexpr "#t");;
+
+let test string = 
+tag_parse (Reader.read_sexpr string);;
+
+let test_reader string = 
+Reader.read_sexpr string;;
 
 end;; (* struct Tag_Parser *)
-
-
-
-
-
-
-
-
 
