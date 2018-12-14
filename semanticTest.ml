b81cac8f4e8d20a9f09290f263bbc4c9693e975d
romayalon <35330373+romayalon@users.noreply.github.com>
2018-12-14 13:49:40 +0200

    Update semanticTest.ml

--- semanticTest.ml
+++ semanticTest.ml
@@ -1,6 +1,5 @@
-(* semantic-analyser.ml
- * A compiler from Scheme to x86/64
- *
+(* semanticTest.ml
+ * Tests for semantic-analyser.ml
  * Programmer: Nitsan Soffair and Amir Abramovich, 2018
  *)
 
@@ -11,7 +10,7 @@ open Tag_Parser;;
 open Reader;;
 open PC;;
 
-(* colors for print *)
+(* Colors for print *)
 let red = "\027[38;5;196m";;
 let grn = "\027[38;5;082m";;
 let yel = "\027[38;5;226m";;
@@ -20,18 +19,17 @@ let purple = "\027[0;35m";;
 let cyan = "\027[0;36m";;
 let reset = "\027[0m";;
 
-
-(* print # number of test that failed *)
+(* Print # number of test that failed *)
 let printFail failed = List.map (fun (n, b) -> Printf.printf "test %s %d %s failed\n" red n reset) failed;;
 
-(* got list of test that failed *)
+(* Got list of test that failed *)
 let getFailed tests = List.filter (fun (n, e) -> e = false) tests;;
 
-(* print summary *)
+(* Print summary *)
 let printSum color typ nfailed nsuccess = 
     (Printf.printf "\n%s%s tests: %s \nfailed- %s%d%s\npassed- %s%d%s\n\n" color typ reset red nfailed reset grn nsuccess reset);;
 
-(* helper function for test, print sum *)
+(* Helper function for test, print sum *)
 let testSum color typ list = 
     let sum = List.length list in
     let failed = (getFailed list) in
@@ -39,12 +37,16 @@ let testSum color typ list =
     let nsuccess = sum - nfailed in
     (printSum color typ nfailed nsuccess);;
 
-(* helper function for test, print test that failed *)
+(* Helper function for test, print test that failed *)
 let testFailed list = 
     let failed = (getFailed list) in
     (printFail failed);;
 
-(* print message *)
+let allPassed color list = 
+  let failed = (getFailed list) in
+  if failed = [] then (Printf.printf "%sALL TESTS PASSED\n%s" color reset);;
+
+(* Print message *)
 let print color txt = (Printf.printf "%s*************************** %s *******************************%s\n" color txt reset);;
 
 (* Compare expr', add support for Box *)
@@ -86,7 +88,7 @@ let rec expr'_eq e1 e2 =
       (List.for_all2 expr'_eq args1 args2)
     | _ -> false;;
 
-print grn "TESTS";;
+print grn "START TESTS";;
 
 
 (* Lexical address tests *)
@@ -208,8 +210,9 @@ let t4 = expr'_eq (annotate_tail_calls(annotate_lexical_addresses (tag_parse_exp
                                                   Applic' (Var' (VarFree "h"), [Var' (VarParam ("y", 1))]),
                                                   Applic' (Var' (VarFree "w"), [Var' (VarParam ("z", 2))]))])));;
 
+(* Change due to Nadav comment: add '\' before '\n' if string *)
 let t5 = expr'_eq (annotate_tail_calls(annotate_lexical_addresses (tag_parse_expression (read_sexpr 
-"(lambda (a b)(f a)(g a b)(display \"done!\n\"))")))) (  LambdaSimple' (["a"; "b"],
+"(lambda (a b)(f a)(g a b)(display \"done!\\n\"))")))) (  LambdaSimple' (["a"; "b"],
                                                         Seq'
                                                         [Applic' (Var' (VarFree "f"), [Var' (VarParam ("a", 0))]);
                                                           Applic' (Var' (VarFree "g"),
@@ -274,7 +277,9 @@ let t10 = expr'_eq (annotate_tail_calls(annotate_lexical_addresses (tag_parse_ex
                                                               [Var' (VarParam ("x", 0))])])]))));;
 
 let t11 = expr'_eq (annotate_tail_calls(annotate_lexical_addresses (tag_parse_expression (read_sexpr 
-"(let ((x (f y)) (y (g x))) (goo (boo x) y))")))) (  Applic'
+"(let ((x (f y))
+      (y (g x))) 
+        (goo (boo x) y))")))) (  Applic'
                                                     (LambdaSimple' (["x"; "y"],
                                                       ApplicTP' (Var' (VarFree "goo"),
                                                       [Applic' (Var' (VarFree "boo"), [Var' (VarParam ("x", 0))]);
@@ -311,6 +316,7 @@ let b2 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
               Applic' (Var' (VarFree "*"),
                 [Var' (VarParam ("x", 0)); Var' (VarParam ("y", 1))])))));;
 
+(* Change due to "box with Seq" Q in Forum *)
 let b3 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
               (define foo3 (lambda (x y) 
                               (lambda () x) 
@@ -320,10 +326,12 @@ let b3 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                               LambdaSimple' (["x"; "y"],
                                Seq'
                                 [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
-                                 LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
-                                 LambdaSimple' ([], Var' (VarBound ("y", 0, 1)));
-                                 LambdaSimple' ([],
-                                  BoxSet' (VarBound ("x", 0, 0), Var' (VarBound ("y", 0, 1))))])));;
+                                 Seq'
+                                  [LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
+                                   LambdaSimple' ([], Var' (VarBound ("y", 0, 1)));
+                                   LambdaSimple' ([],
+                                    BoxSet' (VarBound ("x", 0, 0), Var' (VarBound ("y", 0, 1))))]])));;
+                        
 
 let b4 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
           (define foo4 (lambda (x y)
@@ -331,14 +339,14 @@ let b4 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                                   (set! y x))
                               (lambda (z) (set! x z)))))")))
           (Def' (Var' (VarFree "foo4"),
-          LambdaSimple' (["x"; "y"],
-           Seq'
-            [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
-             If' (BoxGet' (VarParam ("x", 0)),
-              LambdaSimple' ([],
-               Set' (Var' (VarBound ("y", 0, 1)), BoxGet' (VarBound ("x", 0, 0)))),
-              LambdaSimple' (["z"],
-               BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0)))))])));;
+        LambdaSimple' (["x"; "y"],
+          Seq'
+          [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
+            If' (BoxGet' (VarParam ("x", 0)),
+            LambdaSimple' ([],
+              Set' (Var' (VarBound ("y", 0, 1)), BoxGet' (VarBound ("x", 0, 0)))),
+            LambdaSimple' (["z"],
+              BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0)))))])));;
 
 let b5 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
           (define foo5
@@ -443,13 +451,13 @@ let b10 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                 LambdaSimple' (["z"],
                  BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0))))])]));;
 
+(* Change due to "box with Seq" Q in Forum *)
 let b11 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
             (lambda (x y)
               (list (lambda () x)
               (lambda (z) (set! y z))
-              (lambda (z) (set! x z))))
-            ")))
-            (  LambdaSimple' (["x"; "y"],
+              (lambda (z) (set! x z))))")))
+            (LambdaSimple' (["x"; "y"],
             Seq'
              [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
               ApplicTP' (Var' (VarFree "list"),
@@ -459,23 +467,25 @@ let b11 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                 LambdaSimple' (["z"],
                  BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0))))])]));;
 
+(* Change due to "box with Seq" Q in Forum *)
 let b12 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
             (lambda (x y)
             (list (lambda () x) (lambda () y)
             (lambda (z) (set! y z))
             (lambda (z) (set! x z))))
             ")))
-            (  LambdaSimple' (["x"; "y"],
+            (LambdaSimple' (["x"; "y"],
             Seq'
              [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
-              Set' (Var' (VarParam ("y", 1)), Box' (VarParam ("y", 1)));
-              ApplicTP' (Var' (VarFree "list"),
-               [LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
-                LambdaSimple' ([], BoxGet' (VarBound ("y", 0, 1)));
-                LambdaSimple' (["z"],
-                 BoxSet' (VarBound ("y", 0, 1), Var' (VarParam ("z", 0))));
-                LambdaSimple' (["z"],
-                 BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0))))])]));;
+              Seq'
+               [Set' (Var' (VarParam ("y", 1)), Box' (VarParam ("y", 1)));
+                ApplicTP' (Var' (VarFree "list"),
+                 [LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
+                  LambdaSimple' ([], BoxGet' (VarBound ("y", 0, 1)));
+                  LambdaSimple' (["z"],
+                   BoxSet' (VarBound ("y", 0, 1), Var' (VarParam ("z", 0))));
+                  LambdaSimple' (["z"],
+                   BoxSet' (VarBound ("x", 0, 0), Var' (VarParam ("z", 0))))])]]));;
 
 let b13 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
       (define (func .  x)
@@ -508,6 +518,7 @@ let b14 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                                       LambdaSimple' (["z"],
                                        Set' (Var' (VarBound ("x", 1, 0)), Var' (VarParam ("z", 0))))])))));;
 
+(* Change due to "box with Seq" Q in Forum *)                                      
 let b15 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
           (define func (lambda (x y z w)
                           (list (lambda () x)
@@ -518,30 +529,42 @@ let b15 = expr'_eq (run_semantics (tag_parse_expression (read_sexpr "
                                 (lambda () (set! y 1))
                                 (lambda () (set! z 2))
                                 (lambda () (set! w 3)))))")))
-                                (Def' (Var' (VarFree "func"),
+                                (  Def' (Var' (VarFree "func"),
                                 LambdaSimple' (["x"; "y"; "z"; "w"],
-                                  Seq'
+                                 Seq'
                                   [Set' (Var' (VarParam ("x", 0)), Box' (VarParam ("x", 0)));
-                                    Set' (Var' (VarParam ("y", 1)), Box' (VarParam ("y", 1)));
-                                    Set' (Var' (VarParam ("z", 2)), Box' (VarParam ("z", 2)));
-                                    Set' (Var' (VarParam ("w", 3)), Box' (VarParam ("w", 3)));
-                                    ApplicTP' (Var' (VarFree "list"),
-                                    [LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
-                                      LambdaSimple' ([], BoxGet' (VarBound ("y", 0, 1)));
-                                      LambdaSimple' ([], BoxGet' (VarBound ("z", 0, 2)));
-                                      LambdaSimple' ([], BoxGet' (VarBound ("w", 0, 3)));
-                                      LambdaSimple' ([],
-                                      BoxSet' (VarBound ("x", 0, 0), Const' (Sexpr (Number (Int 0)))));
-                                      LambdaSimple' ([],
-                                      BoxSet' (VarBound ("y", 0, 1), Const' (Sexpr (Number (Int 1)))));
-                                      LambdaSimple' ([],
-                                      BoxSet' (VarBound ("z", 0, 2), Const' (Sexpr (Number (Int 2)))));
-                                      LambdaSimple' ([],
-                                      BoxSet' (VarBound ("w", 0, 3), Const' (Sexpr (Number (Int 3)))))])])));;
+                                   Seq'
+                                    [Set' (Var' (VarParam ("y", 1)), Box' (VarParam ("y", 1)));
+                                     Seq'
+                                      [Set' (Var' (VarParam ("z", 2)), Box' (VarParam ("z", 2)));
+                                       Seq'
+                                        [Set' (Var' (VarParam ("w", 3)), Box' (VarParam ("w", 3)));
+                                         ApplicTP' (Var' (VarFree "list"),
+                                          [LambdaSimple' ([], BoxGet' (VarBound ("x", 0, 0)));
+                                           LambdaSimple' ([], BoxGet' (VarBound ("y", 0, 1)));
+                                           LambdaSimple' ([], BoxGet' (VarBound ("z", 0, 2)));
+                                           LambdaSimple' ([], BoxGet' (VarBound ("w", 0, 3)));
+                                           LambdaSimple' ([],
+                                            BoxSet' (VarBound ("x", 0, 0),
+                                             Const' (Sexpr (Number (Int 0)))));
+                                           LambdaSimple' ([],
+                                            BoxSet' (VarBound ("y", 0, 1),
+                                             Const' (Sexpr (Number (Int 1)))));
+                                           LambdaSimple' ([],
+                                            BoxSet' (VarBound ("z", 0, 2),
+                                             Const' (Sexpr (Number (Int 2)))));
+                                           LambdaSimple' ([],
+                                            BoxSet' (VarBound ("w", 0, 3),
+                                             Const' (Sexpr (Number (Int 3)))))])]]]])));;
 
 let boxSet_test = [(1, b1); (2, b2); (3, b3); (4, b4); (5, b5); (6, b6); (7, b7); (8, b8); (9, b9); (10, b10); (11, b11); (12, b12);
-                   (13, b13); (14, b14); (15, b15);];;
+                   (13, b13); (14, b14); (15, b15)];;
 (testSum purple "Box set" boxSet_test);;
 (testFailed boxSet_test);;
 
-print grn "TESTS";;
+(* All tests *)
+let all_test = annotate_test @ tailcalls_test @ boxSet_test;;
+(testSum yel "All" all_test);;
+allPassed cyan all_test;;
+
+print grn "END TESTS";;
