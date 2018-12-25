3f87ba78eb0aadd7628bdb583bb85136c53c5c1c
Tal Baum <talbaum16@gmail.com>
2018-12-25 17:19:10 +0200

    Update code-gen.ml

--- code-gen.ml
+++ code-gen.ml
@@ -1,9 +1,11 @@
 #use "semantic-analyser.ml";;
 
 module type CODE_GEN = sig
-  val make_consts_tbl : expr' list -> (constant * ('a * string)) list
-  val make_fvars_tbl : expr' list -> (string * 'a) list
-  val generate : (constant * ('a * string)) list -> (string * 'a) list -> expr' -> string
+  val make_consts_tbl : expr' list -> (constant * int * string) list
+  (*  val make_consts_tbl : expr' list -> (constant * ('a * string) list*)
+  val make_fvars_tbl : expr' list -> (string * int) list
+  (*  val make_fvars_tbl : expr' list -> (string * 'a) list *)
+  val generate : (constant * int * string) list -> (string * int) list -> expr' -> string
 end;;
 
 module Code_Gen : CODE_GEN = struct
@@ -13,31 +15,32 @@ module Code_Gen : CODE_GEN = struct
   let list_to_string s =
     String.concat "" (List.map (fun str ->  str) s);;
 
-  let rec collect_all_sexprs asts sexpr_list = match asts with
-  | Const'(e) -> [sexpr_list] @ [e] (* verify order *)
-  | If' (testExp , thenExp , elseExp) -> collect_all_sexprs testExp sexpr_list @ collect_all_sexprs thenExp sexpr_list @ collect_all_sexprs elseExp sexpr_list 
-  | Seq'(expr_list) ->  map_collect_sexprs expr_list sexpr_list 
-  | Set'(name , value) -> collect_all_sexprs name sexpr_list @ collect_all_sexprs value sexpr_list 
-  | Def' (name , value) -> collect_all_sexprs name sexpr_list @ collect_all_sexprs value sexpr_list 
-  | Or'(expr_list) -> map_collect_sexprs  expr_list sexpr_list 
-  | LambdaSimple' (args, body) ->  collect_all_sexprs body sexpr_list (*check if should do for args too *)
-  | LambdaOpt' (args, vs, body) -> collect_all_sexprs body sexpr_list
-  | Applic' (function_name , args) -> collect_all_sexprs function_name sexpr_list @ map_collect_sexprs args sexpr_list
-  | ApplicTP' (function_name , args) -> collect_all_sexprs function_name sexpr_list @ map_collect_sexprs args sexpr_list
-  | Box' (e)-> type_of_var e sexpr_list
-  | BoxGet' (e) -> type_of_var e sexpr_list
-  | BoxSet'(e,p) ->  type_of_var e sexpr_list @ collect_all_sexprs p sexpr_list
-  | Var'(e) -> type_of_var e sexpr_list
-  |_ -> []
-  
-  and map_collect_sexprs list sexpr_list  =
-   let after_map_list = List.map (fun(element) -> collect_all_sexprs element sexpr_list) list in
-  List.flatten after_map_list
+  let rec collect_all_sexprs asts sexpr_list = 
+  (match asts with
+  | [] -> []
+  |(ast::others) -> 
+  (match ast with
+  | Const'(e) -> [sexpr_list] @ [e] @ (collect_all_sexprs others sexpr_list) (* verify order *)
+  | If' (testExp , thenExp , elseExp) -> collect_all_sexprs [testExp] sexpr_list @ collect_all_sexprs [thenExp] sexpr_list @ collect_all_sexprs [elseExp] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Seq'(expr_list) ->  collect_all_sexprs expr_list sexpr_list @ (collect_all_sexprs others sexpr_list)  
+  | Set'(name , value) -> collect_all_sexprs [name] sexpr_list @ collect_all_sexprs [value] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Def' (name , value) -> collect_all_sexprs [name] sexpr_list @ collect_all_sexprs [value] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Or'(expr_list) -> collect_all_sexprs  expr_list sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | LambdaSimple' (args, body) ->  collect_all_sexprs [body] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | LambdaOpt' (args, vs, body) -> collect_all_sexprs [body] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Applic' (function_name , args) -> collect_all_sexprs [function_name] sexpr_list @ collect_all_sexprs args sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | ApplicTP' (function_name , args) -> collect_all_sexprs [function_name] sexpr_list @ collect_all_sexprs args sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Box' (e)-> type_of_var [e] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | BoxGet' (e) -> type_of_var [e] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | BoxSet'(e,p) ->  type_of_var [e] sexpr_list @ collect_all_sexprs [p] sexpr_list @ (collect_all_sexprs others sexpr_list) 
+  | Var'(e) -> type_of_var [e] sexpr_list @ (collect_all_sexprs others sexpr_list) ))
+
 
   and type_of_var e sexpr_list= match e with
-  | VarBound(name, major,minor) -> [sexpr_list] @ [Sexpr(String(name))] 
-  | VarParam(name, minor) -> [sexpr_list] @ [Sexpr(String(name))] 
-  | VarFree(name) -> [sexpr_list] @ [Sexpr(String(name))] 
+  | [VarBound(name, major,minor)] -> [sexpr_list] @ [Sexpr(String(name))] 
+  | [VarParam(name, minor)] -> [sexpr_list] @ [Sexpr(String(name))] 
+  | [VarFree(name)] -> [sexpr_list] @ [Sexpr(String(name))] 
+  | _ -> []
 ;;
 
 (* let tmp element copy_list = 
@@ -115,13 +118,13 @@ let rec get_represent elem constant_table= match elem with
   | Sexpr(Nil) -> "MAKE_NIL"
   | Void -> "MAKE_VOID"
   | Sexpr(Bool(e)) -> if e = true then "MAKE_BOOL(1)" else "MAKE_BOOL(0)"
-  | Sexpr(Char(e)) -> "MAKE_CHAR(" ^ (Char.escaped e) ^ ")"
-  | Sexpr(Number(Int(e))) -> "MAKE_INT(" ^ (string_of_int e) ^ ")"
-  | Sexpr(Number(Float(e))) -> "MAKE_FLOAT(" ^(string_of_float e) ^ ")"
-  | Sexpr(Symbol (e) as tmp) ->  "MAKE_LITERAL_SYMBOL(" ^ (find_element tmp constant_table) ^  ")"                             (*implement this*)
-  | Sexpr(String (e)) -> "MAKE_STRING(" ^ e ^ ")"
-  | Sexpr(Pair (car,cdr)) -> "MAKE_LITERAL_PAIR(" ^ (find_element car constant_table) ^ "," ^ (find_element cdr constant_table) ^ ")"
-  | Sexpr(Vector (sexprs_list))-> "MAKE_VECTOR(" ^ 
+  | Sexpr(Char(e)) -> "MAKE_LITERAL_CHAR(\'" ^ (Char.escaped e) ^ "\')"
+  | Sexpr(Number(Int(e))) -> "MAKE_LITERAL_INT(" ^ (string_of_int e) ^ ")"
+  | Sexpr(Number(Float(e))) -> "MAKE_LITERAL_FLOAT(" ^(string_of_float e) ^ ")"
+  | Sexpr(Symbol (e) as tmp) ->  "MAKE_LITERAL_SYMBOL(consts+" ^ (find_element tmp constant_table) ^  ")"                             (*implement this*)
+  | Sexpr(String (e)) -> "MAKE_LITERAL_STRING \"" ^ e ^ "\""
+  | Sexpr(Pair (car,cdr)) -> "MAKE_LITERAL(consts+" ^ (find_element car constant_table) ^ ",consts+" ^ (find_element cdr constant_table) ^ ")"
+  | Sexpr(Vector (sexprs_list))-> "MAKE_LITERAL_VECTOR(" ^ 
       (list_to_string(List.map (fun(elem) -> get_represent (Sexpr(elem)) constant_table) sexprs_list )) ^ ")"
 ;;
 
@@ -138,54 +141,115 @@ List.hd rev_list;;
 (*---------------------------- Free var table -----------------------------*)
 
 
- let rec collect_all_fvars asts fvar_list = match asts with
-  (* | Const'(e) -> if const is relavnt *)
-  | If' (testExp , thenExp , elseExp) -> collect_all_fvars testExp fvar_list @ collect_all_fvars thenExp fvar_list @ collect_all_fvars elseExp fvar_list 
-  | Seq'(expr_list) ->  map_collect_fvars expr_list fvar_list 
-  | Set'(name , value) -> collect_all_fvars name fvar_list @ collect_all_fvars value fvar_list 
-  | Def' (name , value) -> collect_all_fvars name fvar_list @ collect_all_fvars value fvar_list 
-  | Or'(expr_list) -> map_collect_fvars  expr_list fvar_list 
-  | LambdaSimple' (args, body) ->  collect_all_fvars body fvar_list (*check if should do for args too *)
-  | LambdaOpt' (args, vs, body) -> collect_all_fvars body fvar_list
-  | Applic' (function_name , args) -> collect_all_fvars function_name fvar_list @ map_collect_fvars args fvar_list
-  | ApplicTP' (function_name , args) -> collect_all_fvars function_name fvar_list @ map_collect_fvars args fvar_list
-  | Box' (e)-> type_of_var e fvar_list
-  | BoxGet' (e) -> type_of_var e fvar_list
-  | BoxSet'(e,p) ->  type_of_var e fvar_list @ collect_all_fvars p fvar_list
-  | Var'(e) -> type_of_var e fvar_list
-  |_ -> []
-  
-  and map_collect_fvars list fvar_list  =
-   let after_map_list = List.map (fun(element) -> collect_all_fvars element fvar_list) list in
-  List.flatten after_map_list
+ let rec collect_all_fvars asts fvar_list =   match asts with
+  | [] -> []
+  |(ast::others) -> 
+  (match ast with
+  | Const'(e) -> collect_all_fvars others fvar_list 
+  | If' (testExp , thenExp , elseExp) -> collect_all_fvars [testExp] fvar_list @ collect_all_fvars [thenExp] fvar_list @ collect_all_fvars [elseExp] fvar_list @ collect_all_fvars others fvar_list
+  | Seq'(expr_list) ->  collect_all_fvars expr_list fvar_list @ collect_all_fvars others fvar_list
+  | Set'(name , value) -> collect_all_fvars [name] fvar_list @ collect_all_fvars [value] fvar_list @ collect_all_fvars others fvar_list
+  | Def' (name , value) -> collect_all_fvars [name] fvar_list @ collect_all_fvars [value] fvar_list @ collect_all_fvars others fvar_list
+  | Or'(expr_list) -> collect_all_fvars  expr_list fvar_list @ collect_all_fvars others fvar_list
+  | LambdaSimple' (args, body) ->  collect_all_fvars [body] fvar_list @ collect_all_fvars others fvar_list
+  | LambdaOpt' (args, vs, body) -> collect_all_fvars [body] fvar_list @ collect_all_fvars others fvar_list
+  | Applic' (function_name , args) -> collect_all_fvars [function_name] fvar_list @ collect_all_fvars args fvar_list @ collect_all_fvars others fvar_list
+  | ApplicTP' (function_name , args) -> collect_all_fvars [function_name] fvar_list @ collect_all_fvars args fvar_list @ collect_all_fvars others fvar_list
+  | Box' (e)-> type_of_var [e] fvar_list @ collect_all_fvars others fvar_list
+  | BoxGet' (e) -> type_of_var [e] fvar_list @ collect_all_fvars others fvar_list 
+  | BoxSet'(e,p) ->  type_of_var [e] fvar_list @ collect_all_fvars [p] fvar_list @ collect_all_fvars others fvar_list
+  | Var'(e) -> type_of_var [e] fvar_list @ collect_all_fvars others fvar_list)
 
   and type_of_var e fvar_list= match e with
-  | VarBound(name, major,minor) -> [] 
-  | VarParam(name, minor) -> fvar_list @ [name] 
-  | VarFree(name) -> fvar_list @ [name] 
+  | [VarBound(name, major,minor)] -> [] 
+  | [VarParam(name, minor)] -> (*fvar_list @ [name]*) [] 
+  | [VarFree(name)] -> fvar_list @ [name] 
+  | _ -> []
 ;;
 
-let create_label fvar = raise X_not_yet_implemented;;
+let init_const_table = [(Void, 0, "MAKE_VOID");
+(Sexpr(Nil), 1, "MAKE_NIL");
+(Sexpr(Bool false), 2, "MAKE_BOOL(0)");
+(Sexpr(Bool true), 4, "MAKE_BOOL(1)");];;
 
 let rec create_fvar_table fvar_set index = match fvar_set with
 | [] -> []
 | [x] -> [(x,index)]
 | hd::tl -> [(hd,index)] @ (create_fvar_table tl (index + 1)) ;;
 
-(* List.map(fun(fvar)-> (fvar, (create_label fvar)))  fvar_set *)
 
   let make_consts_tbl asts = 
   let constants_list = collect_all_sexprs asts (Sexpr(Nil)) in
   let constants_set  = remove_duplicate constants_list in
   let expanded_list = build_topolig_list constants_set in
   let no_dups_list = remove_duplicate expanded_list in
-  create_const_table (List.flatten no_dups_list) [];; 
+  let initialzied = init_const_table in 
+  create_const_table (List.flatten no_dups_list) initialzied;; 
   
   let make_fvars_tbl asts = 
   let fvar_list = collect_all_fvars asts [] in
   let fvar_set = remove_duplicate fvar_list in 
   create_fvar_table fvar_set 0
   ;;
+  
   let generate consts fvars e = raise X_not_yet_implemented;;
+ (* Printf.sprintf   *)
+  (* let generate consts fvars e = match e with
+  | Const'(x)->"mov rax,AddressInConstTable(" ^ x ^")" 
+  | Var'(VarFree(str)) ->"mov rax, qword [LabelInFVarTable(" ^ str ^")]"
+  | Var'(VarParam (str , minor)) -> "mov rax, qword [rbp + 8 ∗ (4 + minor)]"
+  | Var'(VarBound (str ,major, minor)) ->"mov rax, qword [rbp + 8 ∗ 2]
+  mov rax, qword [rax + 8 ∗ major]
+  mov rax, qword [rax + 8 ∗ minor]"
+  (* | Box' of var *) (*  /////////////// check if need to implement*)
+  | BoxGet'(Var'(v) as vari)-> (generate consts fvars vari ) ^ "\n mov rax, qword [rax]"
+  | BoxSet'(Var'(v) as vari ,value) -> 
+    let  value_text =(generate consts fvars value ) ^ "\n push rax" in
+    let var_text = (generate consts fvars vari ) ^ " \n pop qword [rax]
+    mov rax, sob_void" in
+    value_text ^ var_text
+  | If'(test,dit,dif)->
+    let test_text= (generate consts fvars test ) ^ "cmp rax, sob_false
+      je Lelse \n" in
+    let dit_text = (generate consts fvars dit ) ^ "jmp Lexit
+      Lelse: \n" in 
+    let dif_text = (generate consts fvars dif ) ^ "Lexit:" in
+    test_text ^ dit_text ^ dif_text 
+  | Seq' (list) ->(gen_map list "\n")
+  | Set(Var'(VarParam'(_, minor)),value)-> (generate consts fvars value ) ^ "
+  mov qword [rbp + 8 ∗ (4 + minor)], rax
+  mov rax, sob_void"
+  | Set(Var'(VarBound' (str ,major, minor)),value)->(generate consts fvars value ) ^
+  "mov rbx, qword [rbp + 8 ∗ 2]
+  mov rbx, qword [rbx + 8 ∗ major]
+  mov qword [rbx + 8 ∗ minor], rax
+  mov rax, sob_void"
+  | Set(Var'(VarFree'(str)),value)->"mov qword [LabelInFVarTable(v)], rax
+  mov rax, sob_void"
+  (* | Def' of expr' * expr' *) (*  /////////////// check if need to implement*)
+  | Or'(list)-> ((gen_map list "
+  cmp rax, sob_false
+  jne Lexit") ^ "
+  Lexit:")
+  (* | LambdaSimple' of string list * expr'
+  | LambdaOpt' of string list * string * expr'
+  | Applic' of expr' * (expr' list)
+  | ApplicTP' of expr' * (expr' list);; *)
+  |_->""
+  ;;
+
+  let gen_map list code_to_write = 
+  let mapped = List.map (fun(elem)-> (generate elem) ^ code_to_write) list in
+  (list_to_string mapped) ;; *)
+
+
 end;;
+
+ let test = List.map Semantics.run_semantics
+                         (Tag_Parser.tag_parse_expressions
+                            (Reader.read_sexprs "(list \"ab\" '(1 2) 'c 'ab)"));;
+
+  let freevartest = make_fvars_tbl test ;; 
+  let constvartest = make_consts_tbl test ;; 
+
 
