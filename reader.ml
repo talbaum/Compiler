04b47da872c23ce8d1ef54cee53d61f550acd66d
Tal Baum <talbaum16@gmail.com>
2019-01-08 14:12:21 +0200

    Update reader.ml

--- reader.ml
+++ reader.ml
@@ -33,7 +33,10 @@ let rec sexpr_eq s1 s2 =
   | String(s1), String(s2) -> s1 = s2
   | Symbol(s1), Symbol(s2) -> s1 = s2
   | Pair(car1, cdr1), Pair(car2, cdr2) -> (sexpr_eq car1 car2) && (sexpr_eq cdr1 cdr2)
-  | Vector(l1), Vector(l2) -> List.for_all2 sexpr_eq l1 l2
+  | Vector(l1), Vector(l2) ->
+  if List.length l1 = List.length l2 then
+   List.for_all2 sexpr_eq l1 l2 else
+   false
   | _ -> false;;
   
 module Reader: sig
